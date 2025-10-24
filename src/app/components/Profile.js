71e45ca8bc850ba128bd179024ac356127473550
若林悠斗 <ussr0101ss@gmail.com>
2025-10-24 20:44:51 +0900

    修正

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -2,7 +2,9 @@
 import { getProfile } from "../utils/supabaseFunction"
 import { useEffect } from "react";
 import { useRouter } from 'next/navigation';
+import { useState } from "react";
 const Profile = () => {
+  const [ profile, setProfile] = useState()
   const router = useRouter();
   const clickHandler = () => {
     router.push("/Todo")
@@ -11,12 +13,15 @@ const Profile = () => {
         const plotProfile = async () => {
             try {
                 const fetchedProfile = await getProfile();
+                setProfile(fetchedProfile || []); 
                 // fetchedTodosがundefinedの場合に備えて空配列をセット
             } catch (error) {
                 console.error("データの取得に失敗しました:", error);
             }
           };
           plotProfile();
+          console.log(profile)
+          
       }, []);
   
   return (
@@ -26,6 +31,9 @@ const Profile = () => {
         <div>職業</div>
         <div>達成率</div> 
         <button onClick = {clickHandler}>戻る</button>
+        <div>
+          
+        </div>
     </div>
   )
 }
