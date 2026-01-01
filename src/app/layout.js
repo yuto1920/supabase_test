08a0957581166303d3d799bdb8fea978d029cede
若林悠斗 <ussr0101ss@gmail.com>
2026-01-01 19:13:02 +0900

    vercelの機能追加

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import { AuthProvider } from "../../hooks/state";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
         <AuthProvider>
           <Header />
           {children}
+          <SpeedInsights />
         </AuthProvider>
       </body>
     </html>
